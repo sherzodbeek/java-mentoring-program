b0eb65300257b29f42ee950314a0beb5c77ac5c6
Sherzod Omonov <sherzodbeek98@gmail.com>
2022-09-13 11:32:52 +0500

    trigger updated date time added

---
+++ database/sql-commands/trigger-updated-datetime.sql
@@ -0,0 +1,16 @@
+create function write_update_datetime()
+    returns trigger
+    language plpgsql
+as
+$$
+begin
+    NEW.updated_datetime := CURRENT_TIMESTAMP;
+    return NEW;
+END;
+$$
+
+create TRIGGER write_update_datetime
+    before UPDATE
+    on students
+    for each row
+execute procedure write_update_datetime();
